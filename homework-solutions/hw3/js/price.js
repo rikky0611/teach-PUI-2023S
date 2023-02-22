df1b3f8232609c747e74d2bd29b3b5a5e3f0e8d7
Will Epperson <willepp@live.com>
2023-02-21 23:19:34 -0500

    correct hw3 solution

--- homework-solutions/hw3/js/price.js
+++ homework-solutions/hw3/js/price.js
@@ -1,17 +1,17 @@
 const glazingPrices = {
-	"Keep original" : 0.0,
-	"Sugar milk" : 0.0,
-	"Vanilla milk" : 0.50,
-	"Double chocolate" : 1.50
+	"Keep original": 0.0,
+	"Sugar milk": 0.0,
+	"Vanilla milk": 0.50,
+	"Double chocolate": 1.50
 };
 
 const packPrices = {
-	"1" : 1, "3" : 3, "6" : 5, "12" : 10
+	"1": 1, "3": 3, "6": 5, "12": 10
 };
 
 const basePrice = 2.49;
-let glazingOption = "Keep original";
-let packOption = 1;
+let currentGlazingPrice = 0; // keep original
+let currentPackPrice = 1; // 1
 
 /* Populate glazing options with corresponding price adaptation values */
 const glazingSelect = document.querySelector("select#glazing-options");
@@ -20,7 +20,6 @@ for (const [glazing, price] of Object.entries(glazingPrices)) {
 	const option = document.createElement("option");
 	option.textContent = glazing;
 	option.value = price;
-	console.log(typeof(option.value));
 	glazingSelect.appendChild(option);
 }
 
@@ -36,20 +35,18 @@ for (const [pack, price] of Object.entries(packPrices)) {
 
 /* Record the current glazing option and update the total price */
 function glazingChange(element) {
-	glazingPrice = parseFloat(element.value);
+	currentGlazingPrice = parseFloat(element.value);
 	updateTotalPrice();
 }
 
 /* Record the current pack option and update the total price */
 function packChange(element) {
-	packPrice = parseFloat(element.value);
+	currentPackPrice = parseFloat(element.value);
 	updateTotalPrice();
 }
 
 function updateTotalPrice() {
-	const glazingPrice = glazingPrices[glazingOption];
-	const packPrice = packPrices[packOption];
-	const totalPrice = (basePrice + glazingPrice) * packPrice;
+	const totalPrice = (basePrice + currentGlazingPrice) * currentPackPrice;
 	const totalPriceField = document.querySelector("#add-cart span");
 	totalPriceField.textContent = "$" + totalPrice.toFixed(2);
 }
